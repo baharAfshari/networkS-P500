d6570fe11cd45307676e16455fa8cf52d15c19d4
pooya1991 <pooya1991@gmail.com>
2020-04-18 12:24:54 +0430

    add some changes

--- 6-lagged-cosinus.R
+++ 6-lagged-cosinus.R
@@ -16,16 +16,7 @@ for (day in 1:length(daily_res_lag1)) {
   lag_day_network_sample <- spread(lagday_network_sample[,-1], Ticker2, cos_vecs)
   lag_day_network_sample <- as.matrix(column_to_rownames(lag_day_network_sample, var = "Ticker"))
   sym_lag_1day <- pmax(lag_day_network_sample, t(lag_day_network_sample))
-  mat <- as.matrix(lag_day_network_sample)
-  for (i in 1:length(mat)) {
-    for (j in 1:i+1) {
-      if(mat[i,j]>mat[j,i]){
-        mat[i,j]
-      } else{
-        mat[j,i]
-      }
-    }
-  }
+  
   
   
 }
